13be18545da6bca896d5dc7722d9d544326650fe
Roxog <gabrielcano@hotmail.fr>
2017-11-20 23:17:51 +0100

    gabi : irrigation upgrade + correction postuler.php

--- projet/sql/Insert_datas.sql
+++ projet/sql/Insert_datas.sql
@@ -6,12 +6,12 @@ DELETE from `description`;
 DELETE from `offre`;
 DELETE from `blacklist`;
 
-INSERT INTO `personne` VALUES (1, 'Céler', 'Jack', '1940-06-13', 'F', 'log1', 'mdp1', 'jack.celer@gmail.com');
-INSERT INTO `personne` VALUES (2, 'Sop', 'Alain', '1950-02-13', 'H', 'log2', 'mdp2', 'sop.alain@hotmail.fr');
-INSERT INTO `personne` VALUES (3, 'Sérien', 'Jean', '1983-07-13', 'H', 'log3', 'mdp3', 'jean.serien@free.fr');
-INSERT INTO `personne` VALUES (4, 'Fer', 'Lucie', '1979-03-13', 'F', 'log4', 'mdp4', 'lucie.fer@gmail.com');
-INSERT INTO `personne` VALUES (5, 'Croche', 'Sarah', '1997-04-23', 'F', 'log5', 'mdp5', 'sarah.croche@yahou.com');
-INSERT INTO `personne` VALUES (6, 'Bistruckla', 'César', '1420-05-19', 'H', 'log6', 'mdp6', 'cesar.bistruckla@hotmail.com');
+INSERT INTO `personne` VALUES (1, 'Céler', 'Jack', '1940-06-13', 'F', 'log1', '7373c4286633127542e5c2be2e032aa0498d80b2', 'jack.celer@gmail.com');
+INSERT INTO `personne` VALUES (2, 'Sop', 'Alain', '1950-02-13', 'H', 'log2', 'f747c08c69f65afea969fe3f2da053a192f01d81', 'sop.alain@hotmail.fr');
+INSERT INTO `personne` VALUES (3, 'Sérien', 'Jean', '1983-07-13', 'H', 'log3', '06c5e5c9547af51d43aad1d6402bf4d2ae3ccc37', 'jean.serien@free.fr');
+INSERT INTO `personne` VALUES (4, 'Fer', 'Lucie', '1979-03-13', 'F', 'log4', 'f886993e9b022adcde7a2f1d7e26db221b91a9cc', 'lucie.fer@gmail.com');
+INSERT INTO `personne` VALUES (5, 'Croche', 'Sarah', '1997-04-23', 'F', 'log5', '8a587d3287cc27b6802b5bc49c46ef780623a178', 'sarah.croche@yahou.com');
+INSERT INTO `personne` VALUES (6, 'Bistruckla', 'César', '1420-05-19', 'H', 'log6', '2111184664d46b2b51c589e999a6f3ece3394763', 'cesar.bistruckla@hotmail.com');
 
 INSERT INTO `candidat` VALUES (1, 'Course automobile', 'DUT',5, 1, 1);
 INSERT INTO `candidat` VALUES (2, 'Netoyage', 'Bac',3, 0, 3);
